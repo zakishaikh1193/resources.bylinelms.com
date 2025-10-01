092f71443b6606e78db0c518d76b1efb084f7cc4
Zaki <zaki.byline@gmail.com>
2025-10-01 12:16:43 +0530

    final before Card UI change

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,8 @@
 // API Configuration
 
 
-export const API_BASE_URL = 'https://resources.bylinelms.com/api';
-// export const API_BASE_URL = 'http://localhost:5000/api';
+// export const API_BASE_URL = 'https://resources.bylinelms.com/api';
+export const API_BASE_URL = 'http://localhost:5000/api';
 
 
 export const API_ENDPOINTS = {
